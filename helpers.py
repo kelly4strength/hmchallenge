8589b01c660b356e3a857e964642dce42aac1ecd
Kelly Hoffer <kellyhoffer@Kellys-MacBook-Pro-2.local>
2017-02-20 12:12:56 -0800

    cleaning up and added start of a model for classes. Want to make the game easy to read and alter.

--- helpers.py
+++ helpers.py
@@ -1,9 +1,50 @@
 # helpers
 
+from hmchallenge import 
+import re
+
 def six_guesses(guesses):
-	""" function to count and limit user to 6 guesses """
+	""" function to count and limit user to 6 guesses """  
+	for guesses in range 6:
+
+	if guesses = 6:
+		break
 
 
 def show_word(guess_letter):
 	""" function to show the word being filled in (in progress) """
+# _ _ _ _ _ is word is "hello" and user picks "l" should show _ _ l l _
+	for char in word:
+
+
+
+def confirm_letters(word):
+	"""function to confirm input is letters, that the input doesn's 
+	exceed the char in the word and allows for either case"""
+
+	# maybe a try and except here
+
+
+input_str = raw_input("Please provide some info: ")
+if not re.match("^[a-z]*$", input_str):
+    print "Error! Only letters a-z allowed!"
+    sys.exit()
+elif len(input_str) > 15:
+    print "Error! Only 15 characters allowed!"
+    sys.exit()
+
+print "Your input was:", input_str
+
+def hints():
+	""" function to give an option for a hint at the 5th and 6th guesses """
+
+
+			# shouldn't this just be len(word)
+# def number_of_letters_in_word(word):
+# 	""" function to determine number of letters in word """
+
+
+
+
+
 
--- hmchallenge.py
+++ hmchallenge.py
@@ -1,76 +1,64 @@
-# A game of hangman
-# which can be played by a user against the computer
 
-# secret-keeper is the computer
-# user is guesser
-# they get 6 guesses
+# THINGS TO KEEP TRACK OF:
 
-# if the guesses a letter which is part of the word, 
-# the secret keeper will reveal all occurences  of that letter in the word 
+# right now these are all global but will be made into Class objects
 
-# if the guesser guesser guesses a correct letter such that all letter are now revealed, 
-# the game is over and the guesser has won
-
-# if the guesser runs out of guesses before the whole word is discovered, game is over and computer wins
-
-
-# Game Rules
-#  - At the start of the game, thecomputer/secret-keeper chooses a dictionary word 
-#  	# With what number of letters? something you have to be able to get in 6 guesses???
-#  - the guesser loses the game if they guess 6 letters that are not in the secret word
-#  - the guesser wins the game if they guess all the letters in the secret word correctly 
-#  and have not already lost the game per the conditions above
-#  #  ???
-
-#  User Interface
-#  Any type of user interface is acceptable (command line, mobile app, webpage, etc.) 
-#  but the player must have a way of interacting with yout game, including:
-# - the length of the secret word is displayed to the guesser( like a set of underscores)
-# - as the guesser makes correct guesses, occurences of the guessed letter in the word are 
-# shown while unknown letters are still hidden
-# - The number of guesses remaining is displayed
-# - a list of incorrect guesses are displayed
-
-# Implementation
-# - Your program must retrieve a dictionary list of words from the word dictionary REST API provided
-# http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words
-# - You can use whatever combination of programming languages, tools, frameworks and libraries
-
-# import word_list type thing below
-# http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words
-# or word = random.random_word
-
-			# THINGS TO KEEP TRACK OF:
+# word - for now manually chosen, needs to be hooked up to http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words
+word = "hello"
 # the number of characters in the chosen word, may be multiples of the letters but still same number of chars
+word_length = len(word)
+# to display the word with the correct guesses filled in i.e. "hello" = "_ _ l l _" when "l" is chosen
+# need the character and index to do this
 
+# man = keep track of the body parts 
 
+partial_word = word_length in underscores with correct guesses added
 # defining the variable name for the user input
 guess_letter = None
-
 # number of guesses (must be 6 or under) -will need to add correct and incorrect guesses
-guesses = 6
-
+guesses = 0
 # letters/strings guesses go into the guess list (all the letters?, yes if we have a you already guessed that clause)
 guess_list = []
 
 # player starts with 6 guesses, we need to remove one guess for every legitimate guess (not including letters already guessed)
 # remaining_guesses = 6 
 
+#TESTS
+# input are letters, case doesn't matter
+# input does not exceed number of letters in word
+# tries are limited to 6 (or whatever number you wish)
+# maybe a front end - but letters are put in correct spaces to fill in the word 
+# and all instances of the letter in the word
+
+# if guess == letter in word
+# remove 1 from the 6 guesses
+# add to word list thingie
+# show word thingie to the user
+
+# if guess != letter in word
+# add to guess list
+# remove 1 from the 6 guesses
+
+# if guess == guess in guess_list
+# tell user that has already been guessed and guess again (back to input)
+
+
 def guess_word(word):
 	"""function determines asks user to input guesses """
 
-word = "hello"
-number_chars_in_word = len(word)
-
+# welcome user to game and define the number of letter in the word and number of guesses they get
+# only want to see this once so outside the for loop
 print '''Welcome to Hangman
 		Your word has %d characters in it. 
-		Guess a letter, any letter!''' % (number_chars_in_word)
+		And you have 6 guesses before your man is doomed
+		Guess a letter, any letter!''' % (word_length)
+
+	# if guesses == 6:
+	# 	return "you've run out of guesses"
 
 # while guess_letter not in word:
 # can't use While cause it's a boolean so when it's true, then what?
-
-# THIS IS WHY I'yeah M ONLY GETTING 5 GUESSES!!!
-for guess_letter in guesses:
+for guess_letter in word:
 
 	# prompting the user that keyboard input is expected/requested
 	guess_letter = raw_input("")
@@ -87,18 +75,19 @@ for guess_letter in guesses:
 		
 		if guess_letter not in word:
 			print '''Sorry, that letter is not in the word! 
-					current guesses = %s, and number of letters in word = %s 
-					and you have taken %s guesses. 
+					You have %d guesses left 
+					And %d characters to fill in
 					Try again!\n''' % (guess_list, len(word), guesses) 
 			
 			# counts guesses
 	    	guesses += 1
 
-	    	# NEED TO BREAK OUT OF THE LOOP AND START AGAIN - NEED A LOOP :P!!!
-
-		# if the number given by the user is less than the cats variable    	
 		if guess_letter in word:
-			print "yes that letter is in the word\n "
+			
+			print '''Success, you now have %s 
+					You have %d guesses left 
+					And %d characters to fill in
+					Try again!\n''' % (word, guess_list, len(word), guesses) 
 
 			# counts guesses
 	    	guesses += 1
